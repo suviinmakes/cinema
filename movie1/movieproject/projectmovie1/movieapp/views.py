f3a5ad1ef11bafda19107bec21855c53eb541ce9
suviinmakes <suvikrishnanr2405@gmail.com>
2023-03-23 19:04:23 +0530

    added

---
+++ movie1/movieproject/projectmovie1/movieapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+
+from .models import Movie
+from . forms import MovieForm
+
+
+# Create your views here.
+
+def index(request):
+    movie= Movie.objects.all()
+    context= {
+        'movie_list': movie
+    }
+    return render(request,'index.html', context)
+def detail(request,movie_id):
+    movie= Movie.objects.get (id=movie_id)
+    return render(request, "detail.html", {'movie':movie})
+def add_movie(request):
+    if request.method=="POST":
+        name= request.POST.get('name')
+        discription= request.POST.get('discription')
+        year= request.POST.get('year')
+        image= request.FILES['img']
+        movie= Movie(name=name, discription=discription, year=year, img=image)
+        movie.save()
+    return render(request, 'add.html')
+
+def update(request,id):
+    movie=Movie.objects.get(id=id)
+    form = MovieForm(request.POST or None, request.FILES, instance=movie)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'form': form, 'movie': movie})
+
+def delete(request, id):
+    if request.method=='POST':
+        movie=Movie.objects.get(id=id)
+        movie.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
